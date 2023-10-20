0c9f37f227c5989f3d4e44dc8fb6ca943aaf3cc5
Abilash-devops <abilashdevopsaws@gmail.com>
2023-10-20 12:45:22 +0530

    commit

--- roboshop-infra-standard/02-vpn/provider.tf
+++ roboshop-infra-standard/02-vpn/provider.tf
@@ -7,10 +7,10 @@ terraform {
   }
 
   backend "s3" {
-    bucket   = "roboshop-remote-state"
-    key = "vpn"
+    bucket   = "roboshop-terraform-padmasrikanth"
+    key = "02-vpn"
     region = "us-east-1"
-    dynamodb_table = "roboshop-locking"
+    dynamodb_table = "roboshop"
   }
 }
 
